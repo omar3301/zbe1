9e8bf548810d1fb1663f3f4210844bd037ff105b
omar3301 <68645745+omar3301@users.noreply.github.com>
2020-08-09 01:05:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,25 +6,6 @@ const client3 = new Discord.Client();
 const client4 = new Discord.Client();
 const client5 = new Discord.Client();
 const client6 = new Discord.Client();
-const client7 = new Discord.Client();
-const client8 = new Discord.Client();
-const client9 = new Discord.Client();
-const client10 = new Discord.Client();
-const client11 = new Discord.Client();
-const client12 = new Discord.Client();
-const client13 = new Discord.Client();
-const client14 = new Discord.Client();
-const client15 = new Discord.Client();
-const client16 = new Discord.Client();
-const client17 = new Discord.Client();
-const client18 = new Discord.Client();
-const client19 = new Discord.Client();
-const client20 = new Discord.Client();
-const client21 = new Discord.Client();
-const client22 = new Discord.Client();
-const client23 = new Discord.Client();
-const client24 = new Discord.Client();
-
 
 
 client.login(process.env.TOKEN);
@@ -33,24 +14,85 @@ client3.login(process.env.TOKEN3);
 client4.login(process.env.TOKEN4);
 client5.login(process.env.TOKEN5);
 client6.login(process.env.TOKEN6);
-client7.login(process.env.TOKEN7); 
-client8.login(process.env.TOKEN8); 
-client9.login(process.env.TOKEN9); 
-client10.login(process.env.TOKEN10); 
-client11.login(process.env.TOKEN11); 
-client12.login(process.env.TOKEN12); 
-client13.login(process.env.TOKEN13); 
-client14.login(process.env.TOKEN14); 
-client15.login(process.env.TOKEN15); 
-client16.login(process.env.TOKEN16); 
-client17.login(process.env.TOKEN17); 
-client18.login(process.env.TOKEN18); 
-client19.login(process.env.TOKEN19); 
-client20.login(process.env.TOKEN20); 
-client21.login(process.env.TOKEN21); 
-client22.login(process.env.TOKEN22); 
-client23.login(process.env.TOKEN23); 
-client24.login(process.env.TOKEN24); 
+
+
+client.on('message', message => {
+  if (message.content === 'd') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+
+client2.on('message', message => {
+  if(message.content === 'd'){
+message.channel.send('#daily')
+  }
+  if(message.content === 'c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+
+client3.on('message', message => {
+  if(message.content === 'd'){
+message.channel.send('#daily')
+  }
+  if(message.content === 'c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+
+client4.on('message', message => {
+  if (message.content === 'd') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+
+client5.on('message', message => {
+  if(message.content === 'd'){
+message.channel.send('#daily')
+  }
+  if(message.content === 'c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+
+client6.on('message', message => {
+  if(message.content === 'd'){
+message.channel.send('#daily')
+  }
+  if(message.content === 'c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
 
 
 client.on('ready', async() => {
@@ -60,3 +102,49 @@ var channel = "741732063283249216";
     client.guilds.get(server).channels.get(channel).send('كسم اسامه')
     },50);
 })
+
+
+client2.on('ready', async() => {
+var server = "741732063283249212"; 
+var channel = "741732063283249216";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('كسم اسامه')
+    },50);
+})
+
+
+client3.on('ready', async() => {
+var server = "741732063283249212"; 
+var channel = "741732063283249216";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('كسم اسامه')
+    },50);
+})
+
+
+client4.on('ready', async() => {
+var server = "741732063283249212"; 
+var channel = "741732063283249216";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('كسم اسامه')
+    },50);
+})
+
+
+client5.on('ready', async() => {
+var server = "741732063283249212"; 
+var channel = "741732063283249216";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('كسم اسامه')
+    },50);
+})
+
+
+client6.on('ready', async() => {
+var server = "741732063283249212"; 
+var channel = "741732063283249216";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('كسم اسامه')
+    },50);
+})
+
